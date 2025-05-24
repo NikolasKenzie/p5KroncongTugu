8113cc01732a051b5ac87829d09591ec98117492
Kenzie <nikolaskenz335@gmail.com>
2025-05-24 21:09:03 +0700

    add profile

--- js/index.js
+++ js/index.js
@@ -17,29 +17,42 @@ toggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
+const container = document.querySelector(".home");
+let scrollSpeed = 1; // semakin tinggi semakin cepat
+let scrollInterval;
 
-  const container = document.querySelector('.home');
-  let scrollSpeed = 1; // semakin tinggi semakin cepat
-  let scrollInterval;
+function startAutoScroll() {
+  scrollInterval = setInterval(() => {
+    container.scrollLeft += scrollSpeed;
 
-  function startAutoScroll() {
-    scrollInterval = setInterval(() => {
-      container.scrollLeft += scrollSpeed;
+    // Jika sudah sampai akhir, kembali ke awal
+    if (container.scrollLeft + container.offsetWidth >= container.scrollWidth) {
+      container.scrollLeft = 0;
+    }
+  }, 20); // ubah angka ini buat kecepatan smooth
+}
 
-      // Jika sudah sampai akhir, kembali ke awal
-      if (container.scrollLeft + container.offsetWidth >= container.scrollWidth) {
-        container.scrollLeft = 0;
-      }
-    }, 20); // ubah angka ini buat kecepatan smooth
+function stopAutoScroll() {
+  clearInterval(scrollInterval);
+}
+
+// Mulai auto scroll saat halaman siap
+window.addEventListener("load", startAutoScroll);
+
+// Berhenti sementara kalau mouse hover ke menu
+container.addEventListener("mouseenter", stopAutoScroll);
+container.addEventListener("mouseleave", startAutoScroll);
+
+const containerimg = document.getElementById('auto-scroll-container');
+let scrollAmount = 0;
+const speed = 1; // kecepatan scroll, sesuaikan angka ini
+
+function autoScroll() {
+  scrollAmount += speed;
+  if(scrollAmount >= containerimg.scrollWidth - containerimg.clientWidth) {
+    scrollAmount = 0; // balik ke awal
   }
-
-  function stopAutoScroll() {
-    clearInterval(scrollInterval);
-  }
-
-  // Mulai auto scroll saat halaman siap
-  window.addEventListener('load', startAutoScroll);
-
-  // Berhenti sementara kalau mouse hover ke menu
-  container.addEventListener('mouseenter', stopAutoScroll);
-  container.addEventListener('mouseleave', startAutoScroll);
+  containerimg.scrollLeft = scrollAmount;
+  requestAnimationFrame(autoScroll);
+}
+autoScroll();
---
+++ token.md
@@ -0,0 +1,4 @@
+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
+git add .
+git commit -m "ubah bagian tampilan halaman utama"
+git push
