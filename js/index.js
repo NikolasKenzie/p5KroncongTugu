6c68a0989d3f1be8f627081eefa3aa5b90ac0f26
Kenzie <nikolaskenz335@gmail.com>
2025-05-25 17:32:03 +0700

    adding history

--- js/index.js
+++ js/index.js
@@ -21,38 +21,11 @@ const container = document.querySelector(".home");
 let scrollSpeed = 1; // semakin tinggi semakin cepat
 let scrollInterval;
 
-function startAutoScroll() {
-  scrollInterval = setInterval(() => {
-    container.scrollLeft += scrollSpeed;
 
-    // Jika sudah sampai akhir, kembali ke awal
-    if (container.scrollLeft + container.offsetWidth >= container.scrollWidth) {
-      container.scrollLeft = 0;
-    }
-  }, 20); // ubah angka ini buat kecepatan smooth
+function showPopup() {
+  document.getElementById("popupHistory").style.display = "flex";
 }
 
-function stopAutoScroll() {
-  clearInterval(scrollInterval);
+function closePopup() {
+  document.getElementById("popupHistory").style.display = "none";
 }
-
-// Mulai auto scroll saat halaman siap
-window.addEventListener("load", startAutoScroll);
-
-// Berhenti sementara kalau mouse hover ke menu
-container.addEventListener("mouseenter", stopAutoScroll);
-container.addEventListener("mouseleave", startAutoScroll);
-
-const containerimg = document.getElementById('auto-scroll-container');
-let scrollAmount = 0;
-const speed = 1; // kecepatan scroll, sesuaikan angka ini
-
-function autoScroll() {
-  scrollAmount += speed;
-  if(scrollAmount >= containerimg.scrollWidth - containerimg.clientWidth) {
-    scrollAmount = 0; // balik ke awal
-  }
-  containerimg.scrollLeft = scrollAmount;
-  requestAnimationFrame(autoScroll);
-}
-autoScroll();
